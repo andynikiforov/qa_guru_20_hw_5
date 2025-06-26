016c12ad133c66d1a82991ff7c447e38942c85f8
Andrey Nikiforov <nikiforov@playkot.com>
2025-06-26 19:20:45 +0300

    добавил проверки, заменил xpath на css селекторы

--- test_registration.py
+++ test_registration.py
@@ -1,5 +1,7 @@
+import os
 from selene import browser, have, be
 import pytest
+from selene.core.condition import Condition
 
 
 @pytest.fixture(autouse=True)
@@ -13,6 +15,7 @@ def browser_base_url_and_resolution():
 
 def test_registration_form():
     browser.open('/')
+    # Заполнение формы
     browser.element('#firstName').should(be.visible).type('Ivan')
     browser.element('#lastName').type('Ivanov')
     browser.element('#userEmail').type('name@google.com')
@@ -20,19 +23,38 @@ def test_registration_form():
     browser.element('#userNumber').type('8999456123')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__year-select').click()
-    browser.element('.react-datepicker__year-select option[value="1993"]').click()
+    browser.element('[value="1993"]').click()
     browser.element('.react-datepicker__month-select').click()
-    browser.element('.react-datepicker__month-select option[value="4"]').click()
+    browser.element('[value="4"]').click()
     browser.element('[aria-label="Choose Thursday, May 20th, 1993"]').click()
     browser.element('#subjectsInput').type('History').press_enter()
     browser.element('label[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture').type(os.path.abspath('cat.jpg'))
     browser.element('#currentAddress').type('BII357, Block B 2, Raghubir Nagar, Tagore Garden Extension')
     browser.element('#state').click()
-    browser.element('//div[text()="NCR"]').click()
+    browser.element('#react-select-3-option-2').click()
     browser.element('#city').click()
-    browser.element('//div[text()="Delhi"]').click()
+    browser.element('#react-select-4-option-1').click()
+    # Отправка формы
     browser.element('#submit').click()
+    # Проверки
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-
+    browser.all('td').should(
+        have.exact_texts(
+            'Student Name', 'Ivan Ivanov',
+            'Student Email', 'name@google.com',
+            'Gender', 'Male',
+            'Mobile', '8999456123',
+            'Date of Birth', '20 May,1993',
+            'Subjects', 'History',
+            'Hobbies', 'Sports',
+            'Picture', 'cat.jpg',
+            'Address', 'BII357, Block B 2, Raghubir Nagar, Tagore Garden Extension',
+            'State and City',	'Haryana Panipat',
+        )
+    )
+    browser.element('#closeLargeModal').should(
+        Condition.by_and(be.visible, be.clickable)
+    )
 
 
